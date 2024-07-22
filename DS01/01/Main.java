83ead165bfad644715d589168f98046e8c97088d
venkatk-git <venkatkumar.profiles@gmail.com>
2024-07-22 12:06:05 +0530

    Search a 2D Matrix

--- DS01/01/Main.java
+++ DS01/01/Main.java
@@ -1,20 +1,13 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-bool searchMatrix(int rowLen, int colLen,int matrix[][rowLen], int target);
-
-void main(){
-    int mat[][4] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
-    if(searchMatrix(3, 4, mat, 11)) printf("True");
-    else printf("False");
-    return;
-}
-
-bool searchMatrix(int rowLen, int colLen,int matrix[][rowLen], int target) {
+public class Main {
+    public static void main(String[] args) {
+        int[][] mat = new int[][]{{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        System.out.println(searchMatrix(mat,11));
+    }
+    public static  boolean searchMatrix(int[][] matrix, int target) {
         int rs = 0;
-        int re = rowLen - 1;
+        int re = matrix.length - 1;
         int cs = 0;
-        int ce = colLen - 1;
+        int ce = matrix[0].length - 1;
 
         while (rs <= re) {
             int m1 = (rs + re) / 2;
@@ -34,9 +27,10 @@ bool searchMatrix(int rowLen, int colLen,int matrix[][rowLen], int target) {
                 return false;
             } else if (target < matrix[m1][0]) {
                 re = m1 - 1;
-            } else if (target > matrix[m1][ce]) {
+            } else if (target > matrix[m1][matrix[0].length - 1]) {
                 rs = m1 + 1;
             }
         }
         return false;
+    }
 }
