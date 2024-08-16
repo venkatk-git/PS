316e2883b30e7fb158d9f019943a556176f93504
venkatk-git <venkatkumar.profiles@gmail.com>
2024-08-16 20:37:25 +0530

    Revised Merger function

--- DS01/02/main.c
+++ DS01/02/main.c
@@ -1,12 +1,20 @@
+/*
+Finding a Specific Product in a Sorted Inventory using Binary Search:
+You have a sorted array inventory that contains the names of all the products in the store's inventory.
+A customer comes to you and asks if the store has the product "Wireless Headphones". Your goal is to write
+a function that can efficiently determine whether the requested product is in the
+store's inventory and, if so, return its index in the inventory array using binary search.
+*/
+
 #include <stdio.h>
 #include <string.h>
 
 int search(int n, char *inventory[n], char *target);
 
 void main(){
-    char *inventory[5] = {"Bluetooth Speaker", "Laptop Bag", "Wireless Headphones", "Wired Earbuds", "Wireless Mouse"};
+    char *inventory[5] = {"Bluetooth Speaker", "Laptop Bag", "Wired Earbuds", "Wireless Headphones", "Wireless Mouse"};
 
-    char *target = "Laptop Bag";
+    char *target = "Wired Earbuds";
     int result = search(5, inventory, target);
 
     printf("%d",result);
@@ -14,20 +22,23 @@ void main(){
 }
 
 int search(int n, char *inventory[n], char *target){
-    int l = 0;
-    int r = n;
+    int start = 0;
+    int end = n - 1;
+    int mid;
 
-    while(l <= r){
-        int m = (l + r) / 2;
-        int cmpVal = strcmp(inventory[m],target);
+    while(start <= end){
+        mid = (start + end) / 2;
+        int cmp = strcmp(inventory[mid], target);
+
+        if(cmp == 0)
+            return mid;
         
-        if(cmpVal == 0) return m;
-        else if(cmpVal > 0){
-            r = m - 1;
-        } else{
-            l = m + 1;
-        }
+        if (cmp < 0)
+            start = mid + 1;
+        else
+            end = mid - 1;
     }
 
     return -1;
+
 }
--- DS01/03/main.c
+++ DS01/03/main.c
@@ -1,14 +1,43 @@
+/*
+Merge Two Sorted Arrays
+Given two sorted arrays arr1 and arr2 of sizes m and n respectively, 
+merge them into a single sorted array without using extra space. 
+Assume arr1 has enough space (size of m + n) to hold additional 
+elements from arr2 if needed.
+*/
+
+
 #include <stdio.h>
-#include <string.h>
 
-void numOfAtoms(char *s);
+void merger(int arr1[], int arr2[], int m, int n);
 
-int main(){
-    char *str = "H2SO4";
-    numOfAtoms(str);
+void main(){
+    int arr1[] = {1, 3, 5, 0, 0, 0};
+    int arr2[] = {2, 4, 6};
+
+    merger(arr1, arr2, 6 - 3, 3);
 }
 
-void numOfAtoms(char *s){
+void merger(int arr1[], int arr2[], int m, int n){
+    int i = 0;
+    int j = 0;
+    int k = 0;
 
-    printf("%s", s);
+    int sortedArr[m + n];
+
+    while(i < m && j < n){
+        if(arr1[i] < arr2[j])
+            sortedArr[k++] = arr1[i++];
+        else   
+            sortedArr[k++] = arr2[j++];
+    }
+
+    while(i < m)
+        sortedArr[k++] = arr1[i++];
+
+    while(j < n)
+        sortedArr[k++] = arr2[j++];   
+
+    for(int i = 0; i < m + n; i++)
+        printf("%d ", sortedArr[i]);
 }
